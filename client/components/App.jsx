2b73b47ab8a900bed5214e95f5be326f99653fc7
Christina Phung <christinaphung@Christinas-MBP.lan1>
2022-11-19 14:32:07 -0800

    product cards

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DisplayCard from './displayCard.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <DisplayCard />
+    </div>
+
+  );
+};
+
+export default App;
