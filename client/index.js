806efaf5dd6a53ba2637a3573c6b2c7791a87961
Jay Cheskis <jay.cheskis@gmail.com>
2022-11-18 18:54:25 -0600

    boilerplate code

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App.jsx';
+import './style.css';
+
+render(
+  <App />,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development', 
+  entry: './client/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', `@babel/preset-react`]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      template: 'index.html'
+    })
+  ],
+
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    static: {
+      directory: path.join(__dirname, 'build'),
+      publicPath: '/'
+    },
+
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false
+      }
+    }
+  }
+};
