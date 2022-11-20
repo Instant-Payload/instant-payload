c8df07ba32b2dbaf6ce101d05b93ca66a79716f0
elisanie <73565436+elisanie@users.noreply.github.com>
2022-11-20 13:16:08 -0800

    update navbar

---
+++ client/components/pages/Veggie.js
@@ -0,0 +1,3 @@
+export default function Veggie() {
+    return <h1>veggie</h1>
+}
--- client/index.js
+++ client/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import { render, createRoot } from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
+// import React from 'react';
+// // import { render, createRoot } from 'react-dom/client';
+// // import { BrowserRouter } from 'react-router-dom';
+// import App from './components/App.jsx';
+// import './styles.css';
+
+
+// const root = createRoot(document.getElementById('root'));
+
+// root.render(
+//     <App />
+// );
+
+
+import React from "react";
+import ReactDOM from "react-dom/client"
 import App from './components/App.jsx';
-// import './style.css';
+import './styles.css'
+import { BrowserRouter } from "react-router-dom"
 
-const root = createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-    <App />
-);
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
